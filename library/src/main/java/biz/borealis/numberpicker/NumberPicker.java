6cba17c01c6b1ffe3908b8bf1d4c922abe1574cf
kristijandraca <kristijan.draca@borealis.biz>
2016-10-11 14:01:54 +0200

    merge class

--- library/src/main/java/biz/borealis/numberpicker/NumberPicker.java
+++ library/src/main/java/biz/borealis/numberpicker/NumberPicker.java
@@ -1,22 +1,31 @@
 package biz.borealis.numberpicker;
 
+import android.animation.ArgbEvaluator;
+import android.animation.FloatEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NumberPicker extends LinearLayout {
 
+    private float mItemBigHeight;
     private float mItemSmallHeight;
     private int mAllVerticalScroll;
     private int mMin;
     private int mMax;
-    private RecyclerView mRecyclerView;
+    private final RecyclerView mRecyclerView;
     private NumberPickerAdapter mNumberPickerAdapter;
     private float mTextSize;
     private float mTextSizeSelected;
@@ -51,11 +60,11 @@ public class NumberPicker extends LinearLayout {
 
         setMinimumWidth(context.getResources().getDimensionPixelSize(R.dimen.np_min_width));
 
-        mItemSmallHeight = NumberPickerAdapter.getTextViewHeight(context, false, mTextSize, mTextSizeSelected);
-        float itemBigHeight = NumberPickerAdapter.getTextViewHeight(context, true, mTextSize, mTextSizeSelected);
+        mItemSmallHeight = getTextViewHeight(context, false, mTextSize, mTextSizeSelected);
+        mItemBigHeight = getTextViewHeight(context, true, mTextSize, mTextSizeSelected);
 
         mRecyclerView = new RecyclerView(context);
-        int listHeight = (int) (mItemSmallHeight * 2 + itemBigHeight);
+        int listHeight = (int) (mItemSmallHeight * 2 + mItemBigHeight);
         mRecyclerView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, listHeight));
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
         mRecyclerView.setLayoutManager(linearLayoutManager);
@@ -74,7 +83,7 @@ public class NumberPicker extends LinearLayout {
                     if (newState == RecyclerView.SCROLL_STATE_IDLE) {
                         calculatePositionAndScroll();
                     } else if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
-                        if(mNumberPickerAdapter != null){
+                        if (mNumberPickerAdapter != null) {
                             mNumberPickerAdapter.setSelectedItem(-1);
                         }
 
@@ -88,10 +97,7 @@ public class NumberPicker extends LinearLayout {
                 mAllVerticalScroll += dy;
             }
         });
-        mNumberPickerAdapter = new NumberPickerAdapter(context, mMax, mMin, itemBigHeight, mItemSmallHeight);
-        mNumberPickerAdapter.setTextSize(mTextSize, mTextSizeSelected);
-        mNumberPickerAdapter.setAnimateTextSize(mAnimateTextSize);
-        mNumberPickerAdapter.setTextFadeColor(mTextFadeColor);
+        mNumberPickerAdapter = new NumberPickerAdapter(context);
         mRecyclerView.setAdapter(mNumberPickerAdapter);
         mNumberPickerAdapter.setSelectedItem(1);
         addView(mRecyclerView);
@@ -156,6 +162,10 @@ public class NumberPicker extends LinearLayout {
         scrollListToPosition(expectedPosition);
     }
 
+    private static int dp2px(Context context, int dp) {
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics()));
+    }
+
     private void scrollListToPosition(int expectedPosition) {
         float targetScrollPosDate = expectedPosition * mItemSmallHeight;
         final float missingPxDate = targetScrollPosDate - mAllVerticalScroll;
@@ -164,4 +174,151 @@ public class NumberPicker extends LinearLayout {
         }
         mNumberPickerAdapter.setSelectedItem(Math.round(mAllVerticalScroll / mItemSmallHeight) + 1);
     }
+
+    @NonNull
+    private static TextView getTextView(Context context, boolean isBig, float textSize, float textSizeSelected) {
+        TextView number = new TextView(context);
+        number.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        number.setGravity(Gravity.CENTER_HORIZONTAL);
+        if (isBig) {
+            number.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizeSelected);
+        } else {
+            number.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        }
+
+        return number;
+    }
+
+    @NonNull
+    private TextView getTextView(Context context, float textSize, float textSizeSelected) {
+        return getTextView(context, false, textSize, textSizeSelected);
+    }
+
+    public static int getTextViewHeight(Context context, boolean isBig, float textSize, float textSizeSelected) {
+        TextView textView = getTextView(context, isBig, textSize, textSizeSelected);
+        textView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        return textView.getMeasuredHeight();
+    }
+
+    private class NumberPickerAdapter extends RecyclerView.Adapter<NumberPickerAdapter.Holder> {
+
+        /**
+         * Padding
+         */
+        private static final int VIEW_TYPE_PADDING = 0;
+        /**
+         * View
+         */
+        private static final int VIEW_TYPE_ITEM = 1;
+        private Context mContext;
+        private int selectedItem = -1;
+
+        public NumberPickerAdapter(Context context) {
+            this.mContext = context;
+        }
+
+        @Override
+        public NumberPickerAdapter.Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            if (viewType == VIEW_TYPE_ITEM) {
+                TextView number = getTextView(mContext, mTextSize, mTextSizeSelected);
+                return new ItemHolder(number);
+            } else {
+                View paddingView = new View(mContext);
+                RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(dp2px(mContext, 1), (int) mItemSmallHeight);
+                paddingView.setLayoutParams(layoutParams);
+                return new PaddingHolder(paddingView);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(NumberPickerAdapter.Holder holder, int position) {
+
+            if (holder instanceof PaddingHolder) {
+                PaddingHolder paddingHolder = (PaddingHolder) holder;
+                ViewGroup.LayoutParams params = paddingHolder.itemView.getLayoutParams();
+                if (position != 0) {
+                    params.height = (int) (mItemSmallHeight + mItemBigHeight - mItemSmallHeight);
+                } else {
+                    params.height = (int) mItemSmallHeight;
+                }
+            }
+            if (holder instanceof ItemHolder) {
+                final ItemHolder itemHolder = (ItemHolder) holder;
+                itemHolder.number.setText(String.valueOf(mMax - (mMax - mMin) + position - 1)); //minus padding view
+                if (position == selectedItem) {
+                    if (mTextFadeColor) {
+                        final ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), itemHolder.number.getCurrentTextColor(), ContextCompat.getColor(mContext, R.color.np_text_color_selected));
+                        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animator) {
+                                itemHolder.number.setTextColor((Integer) animator.getAnimatedValue());
+                            }
+
+                        });
+                        colorAnimation.start();
+                    }
+                    if (mAnimateTextSize) {
+                        ValueAnimator textSizeAnimation = ValueAnimator.ofObject(new FloatEvaluator(), mTextSize, mTextSizeSelected);
+                        textSizeAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animator) {
+                                itemHolder.number.setTextSize(TypedValue.COMPLEX_UNIT_PX, (Float) animator.getAnimatedValue());
+                            }
+                        });
+                        textSizeAnimation.start();
+                    }
+
+                } else {
+                    itemHolder.number.setTextColor(ContextCompat.getColor(mContext, R.color.np_text_color));
+                    itemHolder.number.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+                }
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            if (position == 0 || position == getItemCount() - 1) {
+                return VIEW_TYPE_PADDING;
+            }
+            return VIEW_TYPE_ITEM;
+        }
+
+        public void setSelectedItem(int selectedItem) {
+            if (selectedItem != this.selectedItem) {
+                this.selectedItem = selectedItem;
+                notifyDataSetChanged();
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mMax - mMin + 3; // calculate number of items plus 2 padding
+        }
+
+        private class PaddingHolder extends Holder {
+
+            private PaddingHolder(View itemView) {
+                super(itemView);
+
+            }
+        }
+
+        private class ItemHolder extends Holder {
+            private TextView number;
+
+            private ItemHolder(View itemView) {
+                super(itemView);
+                number = (TextView) itemView;
+            }
+        }
+
+        class Holder extends RecyclerView.ViewHolder {
+            private Holder(View itemView) {
+                super(itemView);
+            }
+        }
+    }
+
 }
